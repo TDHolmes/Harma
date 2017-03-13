e481dda77f1de8d83583517efcf8574739391cb0
Tyler Holmes <tylerdanielholmes@gmail.com>
2017-03-12 17:14:31 -0700

    stubbing out I2C functions required

---
+++ Pensel/source/I2C.h
@@ -0,0 +1,13 @@
+#ifndef _I2C_H_
+#define _I2C_H_
+
+#include <stdint.h>
+#include "common.h"
+
+ret_t I2C_read_byte(uint8_t I2C_address, uint8_t register_address, uint8_t * value_read_ptr);
+ret_t I2C_read_bytes(uint8_t I2C_address, uint8_t register_address, uint8_t num_bytes, uint8_t * values_read_ptr);
+
+ret_t I2C_write_byte(uint8_t I2C_address, uint8_t register_address, uint8_t value_to_write);
+ret_t I2C_write_bytes(uint8_t I2C_address, uint8_t register_address, uint8_t num_vals, uint8_t * values_to_write_ptr);
+
+#endif /* _I2C_H_ */
