376ded5141e1c688b474d936c389469dec8358e3
Tyler Holmes <tylerdanielholmes@gmail.com>
2017-04-21 22:43:08 -0700

    removing STM_drivers folder in favor of a peared down drivers folder

--- Pensel/source/STM_drivers/STM32Cube_FW_F3_V1.7.0/Projects/STM32373C_EVAL/Applications/IAP/IAP_Binary_Template/Utilities/axftobin.bat
+++
@@ -1,6 +0,0 @@
-@echo off
-echo AXF to BIN file generation ...
-
-"C:\Keil_v5\ARM\ARMCC\bin\fromelf.exe" --bin --output ..\MDK-ARM\STM32373C_EVAL\STM32373C_EVAL_SysTick.bin ..\MDK-ARM\STM32373C_EVAL\STM32373C_EVAL.axf 
-
-echo AXF to BIN file generation completed.
