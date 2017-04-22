ca0bd07450bb716f6b611eeca950c347788d5355
Tyler Holmes <tylerdanielholmes@gmail.com>
2017-04-22 15:22:32 -0700

    adding in a hardware file for hardware specific functions

---
+++ Pensel/source/hardware.c
@@ -0,0 +1,67 @@
+/*
+ *
+ */
+
+// User includes
+#include "common.h"
+
+// STM drivers
+#include "stm32f3xx.h"
+#include "Drivers/stm32f3xx_hal_def.h"
+#include "Drivers/stm32f3xx_hal.h"
+#include "Drivers/stm32f3xx_hal_rcc.h"
+#include "Drivers/stm32f3xx_hal_flash.h"
+
+
+/**
+  * @brief  System Clock Configuration
+  *         The system Clock is configured as follow :
+  *            System Clock source            = PLL (HSE)
+  *            SYSCLK(Hz)                     = 72000000
+  *            HCLK(Hz)                       = 72000000
+  *            AHB Prescaler                  = 1
+  *            APB1 Prescaler                 = 2
+  *            APB2 Prescaler                 = 1
+  *            HSE Frequency(Hz)              = 8000000
+  *            HSE PREDIV                     = 1
+  *            PLLMUL                         = RCC_PLL_MUL9 (9)
+  *            Flash Latency(WS)              = 2
+  * @param  None
+  * @retval None
+  */
+void SystemClock_Config(void)
+{
+    RCC_ClkInitTypeDef RCC_ClkInitStruct;
+    RCC_OscInitTypeDef RCC_OscInitStruct;
+
+    /* Enable HSE Oscillator and activate PLL with HSE as source */
+    RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSE;
+    RCC_OscInitStruct.HSEState = RCC_HSE_ON;
+    RCC_OscInitStruct.HSEPredivValue = RCC_HSE_PREDIV_DIV1;
+    RCC_OscInitStruct.PLL.PLLState = RCC_PLL_ON;
+    RCC_OscInitStruct.PLL.PLLSource = RCC_PLLSOURCE_HSE;
+    RCC_OscInitStruct.PLL.PLLMUL = RCC_PLL_MUL9;
+
+    if (HAL_RCC_OscConfig(&RCC_OscInitStruct) != HAL_OK) {
+        /* Initialization Error */
+        fatal_error_handler();
+    }
+
+    /* Select PLL as system clock source and configure the HCLK, PCLK1 and PCLK2
+    clocks dividers */
+    RCC_ClkInitStruct.ClockType = (RCC_CLOCKTYPE_SYSCLK | RCC_CLOCKTYPE_HCLK | RCC_CLOCKTYPE_PCLK1 | RCC_CLOCKTYPE_PCLK2);
+    RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_PLLCLK;
+    RCC_ClkInitStruct.AHBCLKDivider = RCC_SYSCLK_DIV1;
+    RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV2;
+    RCC_ClkInitStruct.APB2CLKDivider = RCC_HCLK_DIV1;
+    if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_2) != HAL_OK) {
+        /* Initialization Error */
+        fatal_error_handler();
+    }
+}
+
+
+void configure_pins(void)
+{
+    // configure all pins for the Pensel project
+}
---
+++ Pensel/source/hardware.h
@@ -0,0 +1,10 @@
+#ifndef _HARDWARE_H_
+#define _HARDWARE_H_
+
+
+
+void SystemClock_Config(void);
+void configure_pins(void);
+
+
+#endif /* _HARDWARE_H_ */
