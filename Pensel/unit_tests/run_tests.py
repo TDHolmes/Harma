a91212ef3d62df5af4f82d9f6ff6a136bf1a29ed
Tyler Holmes <tylerdanielholmes@gmail.com>
2017-05-04 22:04:55 -0700

    adding in unit testing

---
+++ Pensel/unit_tests/run_tests.py
@@ -0,0 +1,112 @@
+#!/usr/bin/env python
+# -*- coding: UTF-8 -*-
+import subprocess
+import os
+import sys
+from threading import Thread
+from Queue import Queue, Empty
+import time
+
+
+def run_unit_test(source_code_list, output_name, unity_path, verbose=False):
+    BASE_CMD_START = "gcc  -I {0} {0}unity.c ".format(unity_path)
+    # build up the command with the source and output name
+    abs_path_list = [os.path.abspath(src_file) for src_file in source_code_list]
+    cmd = BASE_CMD_START + "{} -o {}".format(" ".join(abs_path_list), output_name)
+    if verbose:
+        cmd += " -D VERBOSE_OUTPUT"
+    stdout, stderr, retval = run_command(cmd)
+
+    # run the output program
+    cmd = "./{}".format(output_name)
+    stdout, stderr, retval = run_command(cmd)
+
+    run_command("rm {}".format(output_name), print_output=verbose)
+
+    return retval
+
+
+def main(verbose, unity_path):
+    retval = 0
+    # run all tests!
+    retval += run_unit_test(["../source/queue.c", "test_queue.c"],
+                            "test_queue", unity_path, verbose)
+    sys.exit(retval)
+
+
+def run_command(cmd, print_output=True):
+    """
+    Kicks off a subprocess to run and accumilate the stdout of the process.
+    """
+
+    # function we can use to queue up the stdout/err output
+    def enqueue_output(out, queue):
+        for line in iter(out.readline, b''):
+            queue.put(line)
+        out.close()
+
+    if print_output:
+        print(" -> {}".format(cmd))
+
+    # ge the process and stdout/err queues put together
+    proc = subprocess.Popen(cmd, shell=True, stdout=subprocess.PIPE,
+                            stderr=subprocess.PIPE)
+    q_stdout = Queue()
+    q_stderr = Queue()
+    t_stdout = Thread(target=enqueue_output, args=(proc.stdout, q_stdout))
+    t_stderr = Thread(target=enqueue_output, args=(proc.stderr, q_stderr))
+    t_stderr.daemon = True  # thread dies with the program
+    t_stdout.daemon = True
+    t_stdout.start()
+    t_stderr.start()
+    stdout = ""
+    stderr = ""
+
+    # read stdout and stderr without blocking
+    finished = False
+    while True:
+        retval = proc.poll()
+        try:
+            line_stdout = ""
+            while True:
+                line_stdout += q_stdout.get_nowait()
+        except Empty:
+            pass
+        # accumilate stdout and print if we should
+        stdout += line_stdout
+        if print_output and line_stdout != "":
+            print(line_stdout.rstrip("\n"))
+
+        try:
+            line_stderr = ""
+            while True:
+                line_stderr += q_stderr.get_nowait()
+        except Empty:
+            pass
+        # accumilate stderr and print if we should
+        stderr += line_stderr
+        if print_output and line_stderr != "":
+            print(line_stderr.rstrip("\n"))
+
+        if finished:
+            return stdout, stderr, retval
+
+        if retval is not None:
+            finished = True
+            # give stdout and stderr time
+            time.sleep(0.25)
+
+
+if __name__ == '__main__':
+    import argparse
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-u", "--unity-path", type=str,
+                        default="/usr/local/resources/tools/Unity/src/",
+                        help="Path to the Unity C Unit testing framework.")
+    parser.add_argument("-v", "--verbose", action="store_true",
+                        help="Verbose output.")
+
+    args = parser.parse_args()
+
+    main(args.verbose, args.unity_path)
---
+++ Pensel/unit_tests/test_queue.c
@@ -0,0 +1,135 @@
+#include "unity.h"
+#include <stdio.h>
+#include "../source/queue.h"
+
+#define QUEUE_SIZE (20)
+
+uint8_t queue_buffer[QUEUE_SIZE];
+queue_t queue_admin;
+
+
+void queue_init(void)
+{
+    queue_admin.head_ind = 0;
+    queue_admin.tail_ind = 0;
+    queue_admin.unread_items = 0;
+    queue_admin.overwrite_count = 0;
+}
+
+
+void test_basicSetGet(void)
+{
+    uint8_t val;
+    queue_init();
+    queue_buffer[queue_admin.head_ind] = 42;
+    queue_increment_head(&queue_admin, QUEUE_SIZE);
+    val = queue_buffer[queue_admin.tail_ind];
+    queue_increment_tail(&queue_admin, QUEUE_SIZE);
+    // do some output if we've defined verbose output
+    #ifdef VERBOSE_OUTPUT
+    printf("\nFunction: %s\n", __func__);
+    printf("\tPut %d onto buffer\n", 42);
+    printf("\tGot %d back\n", val);
+    printf("\n");
+    #endif
+
+    TEST_ASSERT_EQUAL_HEX8(42, val);
+}
+
+
+void test_basicSetIncrementsUnreadCount(void)
+{
+    queue_init();
+    queue_buffer[queue_admin.head_ind] = 42;
+    queue_increment_head(&queue_admin, QUEUE_SIZE);
+    // do some output if we've defined verbose output
+    #ifdef VERBOSE_OUTPUT
+    printf("\nFunction: %s\n", __func__);
+    printf("\tPut %d onto buffer\n", 42);
+    printf("\t%d unread values\n", queue_admin.unread_items);
+    printf("\n");
+    #endif
+
+    TEST_ASSERT_EQUAL_HEX8(1, queue_admin.unread_items);
+}
+
+
+void test_basicGetDecrementsUnreadCount(void)
+{
+    uint8_t val;
+    queue_init();
+    queue_buffer[queue_admin.head_ind] = 42;
+    queue_increment_head(&queue_admin, QUEUE_SIZE);
+    queue_buffer[queue_admin.head_ind] = 24;
+    queue_increment_head(&queue_admin, QUEUE_SIZE);
+    queue_buffer[queue_admin.head_ind] = 84;
+    queue_increment_head(&queue_admin, QUEUE_SIZE);
+    val = queue_buffer[queue_admin.tail_ind];
+    queue_increment_tail(&queue_admin, QUEUE_SIZE);
+    // do some output if we've defined verbose output
+    #ifdef VERBOSE_OUTPUT
+    printf("\nFunction: %s\n", __func__);
+    printf("\tPut 3 values onto buffer and popped off 1\n");
+    printf("\t%d unread values\n", queue_admin.unread_items);
+    printf("\n");
+    #endif
+
+    TEST_ASSERT_EQUAL_HEX8(2, queue_admin.unread_items);
+}
+
+
+
+void test_overwriteCountIncreases(void)
+{
+    queue_init();
+    for (uint8_t i = 0; i <= QUEUE_SIZE; i++) {
+        queue_buffer[queue_admin.head_ind] = i;
+        queue_increment_head(&queue_admin, QUEUE_SIZE);
+    }
+    // do some output if we've defined verbose output
+    #ifdef VERBOSE_OUTPUT
+    printf("\nFunction: %s\n", __func__);
+    printf("\t%d + 1 values onto buffer of size %d\n", QUEUE_SIZE, QUEUE_SIZE);
+    printf("\t%d overwritten values\n", queue_admin.overwrite_count);
+    printf("\n");
+    #endif
+
+    TEST_ASSERT_EQUAL_HEX8(1, queue_admin.overwrite_count);
+}
+
+
+void test_overwrite_getExpectedValue(void)
+{
+    uint8_t val;
+    queue_init();
+    for (uint8_t i = 0; i <= QUEUE_SIZE; i++) {
+        queue_buffer[queue_admin.head_ind] = i;
+        queue_increment_head(&queue_admin, QUEUE_SIZE);
+    }
+    val = queue_buffer[queue_admin.tail_ind];
+    queue_increment_tail(&queue_admin, QUEUE_SIZE);
+    // do some output if we've defined verbose output
+    #ifdef VERBOSE_OUTPUT
+    printf("\nFunction: %s\n", __func__);
+    printf("\t%d + 1 values onto buffer of size %d\n", QUEUE_SIZE, QUEUE_SIZE);
+    printf("\tgot 1 value back == %d\n", val);
+    printf("\n");
+    #endif
+
+    TEST_ASSERT_EQUAL_HEX8(1, val);
+}
+
+
+
+int main(void)
+{
+    UNITY_BEGIN();
+
+    RUN_TEST(test_basicSetGet);
+    RUN_TEST(test_basicSetIncrementsUnreadCount);
+    RUN_TEST(test_basicGetDecrementsUnreadCount);
+    RUN_TEST(test_overwriteCountIncreases);
+    RUN_TEST(test_overwrite_getExpectedValue);
+
+    return UNITY_END();
+}
