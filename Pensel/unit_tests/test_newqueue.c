81f61a6d3d5a3c8289f501f0802b5246688510eb
Tyler Holmes <tylerdanielholmes@gmail.com>
2017-05-21 19:59:39 -0500

    adding more queue tests

--- Pensel/unit_tests/test_newqueue.c
+++ Pensel/unit_tests/test_newqueue.c
@@ -3,6 +3,8 @@
 #include "newqueue.h"
 
 #define QUEUE_SIZE (20)
+#define LARGE_NUMBER (999999)
+
 newqueue_t queue;
 
 
@@ -18,6 +20,7 @@ void test_basicSetGet(void)
     queue_init(QUEUE_SIZE, 1);
     val = 42;
     newqueue_push(&queue, (void *)&val);
+    val = 0;
     newqueue_pop(&queue, (void *)&val, false);
     // do some output if we've defined verbose output
     #ifdef VERBOSE_OUTPUT
@@ -28,6 +31,29 @@ void test_basicSetGet(void)
     #endif
 
     TEST_ASSERT_EQUAL_HEX8(42, val);
+    newqueue_deinit(&queue);
+}
+
+
+void test_basicSetGet_peak(void)
+{
+    uint8_t val;
+    queue_init(QUEUE_SIZE, 1);
+    val = 42;
+    newqueue_push(&queue, (void *)&val);
+    val = 0;
+    newqueue_pop(&queue, (void *)&val, true);
+    // do some output if we've defined verbose output
+    #ifdef VERBOSE_OUTPUT
+        printf("\nFunction: %s\n", __func__);
+        printf("\tPut %d onto buffer\n", 42);
+        printf("\tGot %d back (peak)\n", val);
+        printf("\t%d unread items\n", queue.unread_items);
+        printf("\n");
+    #endif
+
+    TEST_ASSERT_EQUAL_HEX8(42, val);
+    TEST_ASSERT_EQUAL_HEX8(1, queue.unread_items);
     newqueue_deinit(&queue);
 }
 
@@ -68,10 +94,36 @@ void test_basicGetDecrementsUnreadCount(void)
         printf("\nFunction: %s\n", __func__);
         printf("\tPut 3 values onto buffer and popped off 1\n");
         printf("\t%d unread values\n", queue.unread_items);
+        printf("\tPushed %d on first, got %d back\n",42, val);
         printf("\n");
     #endif
 
     TEST_ASSERT_EQUAL_HEX8(2, queue.unread_items);
+    TEST_ASSERT_EQUAL_HEX8(42, val);
+    newqueue_deinit(&queue);
+}
+
+
+void test_fullQueue_pushPop(void)
+{
+    uint8_t val;
+    queue_init(QUEUE_SIZE, 1);
+    for (uint8_t i = 0; i < QUEUE_SIZE; i++) {
+        newqueue_push(&queue, (void *)&i);
+    }
+
+    #ifdef VERBOSE_OUTPUT
+        printf("\nFunction: %s\n", __func__);
+    #endif
+
+    for (uint8_t i = 0; i < QUEUE_SIZE; i++) {
+        newqueue_pop(&queue, (void *)&val, false);
+        #ifdef VERBOSE_OUTPUT
+            printf("\t%d == %d?\n", i, val);
+        #endif
+        TEST_ASSERT_EQUAL_HEX8(i, val);
+    }
+
     newqueue_deinit(&queue);
 }
 
@@ -116,15 +168,87 @@ void test_overwrite_getExpectedValue(void)
 }
 
 
+/************* Large number tests! *****************/
+
+
+void test_fullQueue_pushPop_int32(void)
+{
+    int32_t val;
+    queue_init(QUEUE_SIZE, 4);
+
+    #ifdef VERBOSE_OUTPUT
+        printf("\nFunction: %s\n", __func__);
+    #endif
+
+    // build up queue
+    for (int32_t i = 0; i < QUEUE_SIZE; i++) {
+        val = LARGE_NUMBER + i;
+        // #ifdef VERBOSE_OUTPUT
+        //     printf("\tPushing %ld on\n", val);
+        // #endif
+        newqueue_push(&queue, (void *)&val);
+    }
+
+    // Pop it out and check
+    for (uint8_t i = 0; i < QUEUE_SIZE; i++) {
+        newqueue_pop(&queue, (void *)&val, false);
+        #ifdef VERBOSE_OUTPUT
+            printf("\t%ld == %ld?\n", LARGE_NUMBER + i, val);
+        #endif
+        TEST_ASSERT_EQUAL_HEX32(LARGE_NUMBER + i, val);
+    }
+
+    newqueue_deinit(&queue);
+}
+
+
+void test_fullQueue_pushPop_int32neg(void)
+{
+    int32_t val;
+    queue_init(QUEUE_SIZE, 4);
+
+    #ifdef VERBOSE_OUTPUT
+        printf("\nFunction: %s\n", __func__);
+    #endif
+
+    // build up queue
+    for (int32_t i = 0; i < QUEUE_SIZE; i++) {
+        val = i - LARGE_NUMBER;
+        // #ifdef VERBOSE_OUTPUT
+        //     printf("\tPushing %i on\n", val);
+        // #endif
+        newqueue_push(&queue, (void *)&val);
+    }
+
+    // Pop it out and check
+    for (int32_t i = 0; i < QUEUE_SIZE; i++) {
+        newqueue_pop(&queue, (void *)&val, false);
+        #ifdef VERBOSE_OUTPUT
+            printf("\t%i == %i?\n", (-LARGE_NUMBER) + i, val);
+        #endif
+        TEST_ASSERT_EQUAL((-LARGE_NUMBER) + i, val);
+    }
+
+    newqueue_deinit(&queue);
+}
+
+
 int main(void)
 {
     UNITY_BEGIN();
 
     RUN_TEST(test_basicSetGet);
+    RUN_TEST(test_basicSetGet_peak);
+    RUN_TEST(test_fullQueue_pushPop);
+
     RUN_TEST(test_basicSetIncrementsUnreadCount);
     RUN_TEST(test_basicGetDecrementsUnreadCount);
+
     RUN_TEST(test_overwriteCountIncreases);
     RUN_TEST(test_overwrite_getExpectedValue);
+
+    RUN_TEST(test_fullQueue_pushPop_int32);
+    RUN_TEST(test_fullQueue_pushPop_int32neg);
 
     return UNITY_END();
 }
