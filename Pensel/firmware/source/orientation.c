3e7eae52f6f529bf242ce07e8db46750fab0eac8
Tyler Holmes <tylerdanielholmes@gmail.com>
2017-06-25 23:06:40 -0700

    switching FIR implementation to not malloc and documenting things

--- Pensel/firmware/source/orientation.c
+++ Pensel/firmware/source/orientation.c
@@ -14,53 +14,52 @@
 #include "FIR.h"
 
 // FIR coefficients
-#include "accel_coefficients.h"
-#include "mag_coefficients.h"
+#include "FIR_coefficients.h"
 
 
-#define X_IND (0) //!< When dealing with a 3 element array representing cartesian coordinates, this is the X index
-#define Y_IND (1) //!< When dealing with a 3 element array representing cartesian coordinates, this is the Y index
-#define Z_IND (2) //!< When dealing with a 3 element array representing cartesian coordinates, this is the Z index
+#define X_IND (0) //!< When dealing with a 3 element array for cartesian coords, this is the X index
+#define Y_IND (1) //!< When dealing with a 3 element array for cartesian coords, this is the Y index
+#define Z_IND (2) //!< When dealing with a 3 element array for cartesian coords, this is the Z index
 
 
 typedef struct {
-    FIR_admin_t * FIR_accelGrav_ptr[3];  //!< an FIR filter for all 3 axes of accel for gravity detection
-    FIR_admin_t * FIR_magNorth_ptr[3];   //!< an FIR filter for all 3 axes of mag for north detection
+    FIR_admin_t FIR_accelGrav_ptr[3];    //!< an FIR filter for all 3 axes of accel for gravity detection
+    FIR_admin_t FIR_magNorth_ptr[3];     //!< an FIR filter for all 3 axes of mag for north detection
     quanternion_vector_t north_vector;   //!< Detected north vector in quanternion form
     quanternion_vector_t gravity_vector; //!< Detected gravity vector in quanternion form
     quanternion_vector_t pensel_vector;  //!< Calculated pensel orientation in quanternion form
 } orientation_admin_t;
 
 
-orientation_admin_t orient;
+static orientation_admin_t orient;
 
 
 ret_t orient_init(void)
 {
     ret_t retval;
     // Initialize all 3 vectors for accel gravitation filtering
-    retval = FIR_init(orient.FIR_accelGrav_ptr[X_IND], FIR_ACCEL_GRAVITY_ORDER,
+    retval = FIR_init(&orient.FIR_accelGrav_ptr[X_IND], FIR_ACCEL_GRAVITY_ORDER,
                       accel_coefficients_LPF);
     if (retval != RET_OK) { return retval; }
 
-    retval = FIR_init(orient.FIR_accelGrav_ptr[Y_IND], FIR_ACCEL_GRAVITY_ORDER,
+    retval = FIR_init(&orient.FIR_accelGrav_ptr[Y_IND], FIR_ACCEL_GRAVITY_ORDER,
                       accel_coefficients_LPF);
     if (retval != RET_OK) { return retval; }
 
-    retval = FIR_init(orient.FIR_accelGrav_ptr[Z_IND], FIR_ACCEL_GRAVITY_ORDER,
+    retval = FIR_init(&orient.FIR_accelGrav_ptr[Z_IND], FIR_ACCEL_GRAVITY_ORDER,
                       accel_coefficients_LPF);
     if (retval != RET_OK) { return retval; }
 
     // Initialize all 3 vectors for mag north filtering
-    retval = FIR_init(orient.FIR_magNorth_ptr[X_IND], FIR_MAG_NORTH_ORDER,
+    retval = FIR_init(&orient.FIR_magNorth_ptr[X_IND], FIR_MAG_NORTH_ORDER,
                       mag_coefficients_LPF);
     if (retval != RET_OK) { return retval; }
 
-    retval = FIR_init(orient.FIR_magNorth_ptr[Y_IND], FIR_MAG_NORTH_ORDER,
+    retval = FIR_init(&orient.FIR_magNorth_ptr[Y_IND], FIR_MAG_NORTH_ORDER,
                       mag_coefficients_LPF);
     if (retval != RET_OK) { return retval; }
 
-    retval = FIR_init(orient.FIR_magNorth_ptr[Z_IND], FIR_MAG_NORTH_ORDER,
+    retval = FIR_init(&orient.FIR_magNorth_ptr[Z_IND], FIR_MAG_NORTH_ORDER,
                       mag_coefficients_LPF);
     if (retval != RET_OK) { return retval; }
 
@@ -68,31 +67,58 @@ ret_t orient_init(void)
 }
 
 
-void orient_run(void)
+void orient_calcPenselOrientation(void)
 {
-    // update magnetic north & gravity vectors, if new data available
-    // Calculated pensel orientation if new mag / gravity packets
-}
-
-
-quanternion_vector_t orient_calcPenselOrientation(void)
-{
-    quanternion_vector_t pensel_orientation;
     // Calculate pensel orientation from orient.north_vector and orient.gravity_vector!
-
-    return pensel_orientation;
+    // TODO: update pensel's orientation in the admin struct
 }
 
 
+/*! Takes in a new magnetometer packet and updates the magnetic north orientation
+ *  calculation.
+ *
+ * @param pkt (mag_packet_t): New magnetometer packet from sensor.
+ */
 void orient_calcMagOrientation(mag_packet_t pkt)
 {
     // Update orient.north_vector!
     // TODO: low pass filter on raw mag data to get good north readings
 }
 
-
+/*! Takes in a new accelerometer packet and updates the gravity vector orientation
+ *  calculation.
+ *
+ * @param pkt (accel_packet_t): New accelerometer packet from sensor.
+ */
 void orient_calcAccelOrientation(accel_packet_t pkt)
 {
     // Update orient.gravity_vector!
     // TODO: low pass filter on raw accel data to get good gravity readings
+}
+
+/*! Returns the currently computed pensel orientation (quanternion_vector_t)
+ *
+ * @return pensel_vector: The current pensel vector.
+ */
+quanternion_vector_t orient_getPenselOrientation(void)
+{
+    return orient.pensel_vector;
+}
+
+/*! Returns the currently computed magnetic north orientation (quanternion_vector_t)
+ *
+ * @return pensel_vector: The current north vector.
+ */
+quanternion_vector_t orient_getMagOrientation(void)
+{
+    return orient.north_vector;
+}
+
+/*! Returns the currently computed gravity orientation (quanternion_vector_t)
+ *
+ * @return pensel_vector: The current gravity vector.
+ */
+quanternion_vector_t orient_getAccelOrientation(void)
+{
+    return orient.gravity_vector;
 }
