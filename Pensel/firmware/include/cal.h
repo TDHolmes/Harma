18e2f6f9877d61a9a27840c490fe633eedce217c
Tyler Holmes <tylerdanielholmes@gmail.com>
2017-07-09 20:40:37 -0700

    Initial calibration support code

---
+++ Pensel/firmware/include/cal.h
@@ -0,0 +1,10 @@
+/*!
+ * @file    cal.h
+ * @author  Tyler Holmes
+ * @version 0.1.0
+ * @date    20-May-2017
+ * @brief   Calibration code and structures
+ */
+#pragma once
+
+#include <stdint.h>
---
+++ Pensel/firmware/source/cal.c
@@ -0,0 +1,9 @@
+/*!
+ * @file    cal.c
+ * @author  Tyler Holmes
+ * @version 0.1.0
+ * @date    20-May-2017
+ * @brief   Calibration code and structures
+ */
+
+#include <stdint.h>
