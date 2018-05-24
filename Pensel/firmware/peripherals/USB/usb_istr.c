144d3ba2b5c52746c9e6573c2670a6e3ab8f755a
Tyler Holmes <tylerdanielholmes@gmail.com>
2018-05-24 13:33:25 -0700

    Some USB code cleanup

--- Pensel/firmware/peripherals/USB/usb_istr.c
+++ Pensel/firmware/peripherals/USB/usb_istr.c
@@ -46,7 +46,7 @@
 /* Private define ------------------------------------------------------------*/
 /* Private macro -------------------------------------------------------------*/
 /* Private variables ---------------------------------------------------------*/
-__IO uint16_t wIstr;  /* ISTR register last read value */
+__IO uint16_t wIstr;            /* ISTR register last read value */
 __IO uint8_t bIntPackSOF = 0;   /* SOFs received between 2 consecutive packets */
 __IO uint32_t esof_counter = 0; /* expected SOF counter */
 __IO uint32_t wCNTR = 0;
@@ -82,155 +82,150 @@ void (*pEpInt_OUT[7])(void) = {
 void USB_Istr(void)
 {
     uint32_t i=0;
- __IO uint32_t EP[8];
+    __IO uint32_t EP[8];
 
-  wIstr = _GetISTR();
+    wIstr = _GetISTR();
 
-#if (IMR_MSK & ISTR_SOF)
-  if (wIstr & ISTR_SOF & wInterrupt_Mask)
-  {
-    _SetISTR((uint16_t)CLR_SOF);
-    bIntPackSOF++;
+    #if (IMR_MSK & ISTR_SOF)
+        if (wIstr & ISTR_SOF & wInterrupt_Mask) {
+            _SetISTR((uint16_t)CLR_SOF);
+            bIntPackSOF++;
 
-#ifdef SOF_CALLBACK
-    SOF_Callback();
-#endif
-  }
-#endif
-  /*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*/
+            #ifdef SOF_CALLBACK
+                SOF_Callback();
+            #endif
+        }
+    #endif
+    /*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*/
 
-#if (IMR_MSK & ISTR_CTR)
-  if (wIstr & ISTR_CTR & wInterrupt_Mask)
-  {
-    /* servicing of the endpoint correct transfer interrupt */
-    /* clear of the CTR flag into the sub */
-    CTR_LP();
-#ifdef CTR_CALLBACK
-    CTR_Callback();
-#endif
-  }
-#endif
-  /*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*/
-#if (IMR_MSK & ISTR_RESET)
-  if (wIstr & ISTR_RESET & wInterrupt_Mask)
-  {
-    _SetISTR((uint16_t)CLR_RESET);
-    Device_Property.Reset();
-#ifdef RESET_CALLBACK
-    RESET_Callback();
-#endif
-  }
-#endif
-  /*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*/
-#if (IMR_MSK & ISTR_DOVR)
-  if (wIstr & ISTR_DOVR & wInterrupt_Mask)
-  {
-    _SetISTR((uint16_t)CLR_DOVR);
-#ifdef DOVR_CALLBACK
-    DOVR_Callback();
-#endif
-  }
-#endif
-  /*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*/
-#if (IMR_MSK & ISTR_ERR)
-  if (wIstr & ISTR_ERR & wInterrupt_Mask)
-  {
-    _SetISTR((uint16_t)CLR_ERR);
-#ifdef ERR_CALLBACK
-    ERR_Callback();
-#endif
-  }
-#endif
-  /*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*/
-#if (IMR_MSK & ISTR_WKUP)
-  if (wIstr & ISTR_WKUP & wInterrupt_Mask)
-  {
-    _SetISTR((uint16_t)CLR_WKUP);
-    Resume(RESUME_EXTERNAL);
-#ifdef WKUP_CALLBACK
-    WKUP_Callback();
-#endif
-  }
-#endif
-  /*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*/
-#if (IMR_MSK & ISTR_SUSP)
-  if (wIstr & ISTR_SUSP & wInterrupt_Mask)
-  {
+    #if (IMR_MSK & ISTR_CTR)
+        if (wIstr & ISTR_CTR & wInterrupt_Mask) {
+            /* servicing of the endpoint correct transfer interrupt */
+            /* clear of the CTR flag into the sub */
+            CTR_LP();
+            #ifdef CTR_CALLBACK
+                CTR_Callback();
+            #endif
+        }
+    #endif
 
-    /* check if SUSPEND is possible */
-    if (fSuspendEnabled)
-    {
-      Suspend();
-    }
-    else
-    {
-      /* if not possible then resume after xx ms */
-      Resume(RESUME_LATER);
-    }
-    /* clear of the ISTR bit must be done after setting of CNTR_FSUSP */
-    _SetISTR((uint16_t)CLR_SUSP);
-#ifdef SUSP_CALLBACK
-    SUSP_Callback();
-#endif
-  }
-#endif
-  /*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*/
+    /*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*/
+    #if (IMR_MSK & ISTR_RESET)
+        if (wIstr & ISTR_RESET & wInterrupt_Mask) {
+            _SetISTR((uint16_t)CLR_RESET);
+            Device_Property.Reset();
+            #ifdef RESET_CALLBACK
+                RESET_Callback();
+            #endif
+        }
+    #endif
 
-#if (IMR_MSK & ISTR_ESOF)
-  if (wIstr & ISTR_ESOF & wInterrupt_Mask)
-  {
-    /* clear ESOF flag in ISTR */
-    _SetISTR((uint16_t)CLR_ESOF);
+    /*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*/
+    #if (IMR_MSK & ISTR_DOVR)
+        if (wIstr & ISTR_DOVR & wInterrupt_Mask) {
+            _SetISTR((uint16_t)CLR_DOVR);
+            #ifdef DOVR_CALLBACK
+                DOVR_Callback();
+            #endif
+        }
+    #endif
 
-    if ((_GetFNR()&FNR_RXDP)!=0)
-    {
-      /* increment ESOF counter */
-      esof_counter ++;
+    /*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*/
+    #if (IMR_MSK & ISTR_ERR)
+        if (wIstr & ISTR_ERR & wInterrupt_Mask) {
+            _SetISTR((uint16_t)CLR_ERR);
+            #ifdef ERR_CALLBACK
+                ERR_Callback();
+            #endif
+        }
+    #endif
 
-      /* test if we enter in ESOF more than 3 times with FSUSP =0 and RXDP =1=>> possible missing SUSP flag*/
-      if ((esof_counter >3)&&((_GetCNTR()&CNTR_FSUSP)==0))
-      {
-        /* this a sequence to apply a force RESET*/
+    /*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*/
+    #if (IMR_MSK & ISTR_WKUP)
+        if (wIstr & ISTR_WKUP & wInterrupt_Mask) {
+            _SetISTR((uint16_t)CLR_WKUP);
+            Resume(RESUME_EXTERNAL);
+            #ifdef WKUP_CALLBACK
+                WKUP_Callback();
+            #endif
+        }
+    #endif
 
-        /*Store CNTR value */
-        wCNTR = _GetCNTR();
+    /*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*/
+    #if (IMR_MSK & ISTR_SUSP)
+        if (wIstr & ISTR_SUSP & wInterrupt_Mask) {
 
-        /*Store endpoints registers status */
-        for (i=0;i<8;i++) EP[i] = _GetENDPOINT(i);
+            /* check if SUSPEND is possible */
+            if (fSuspendEnabled) {
+                Suspend();
+            } else {
+                /* if not possible then resume after xx ms */
+                Resume(RESUME_LATER);
+            }
 
-        /*apply FRES */
-        wCNTR|=CNTR_FRES;
-        _SetCNTR(wCNTR);
+            /* clear of the ISTR bit must be done after setting of CNTR_FSUSP */
+            _SetISTR((uint16_t)CLR_SUSP);
+            #ifdef SUSP_CALLBACK
+                SUSP_Callback();
+            #endif
+        }
+    #endif
 
-        /*clear FRES*/
-        wCNTR&=~CNTR_FRES;
-        _SetCNTR(wCNTR);
+    /*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*/
+    #if (IMR_MSK & ISTR_ESOF)
+        if (wIstr & ISTR_ESOF & wInterrupt_Mask) {
+            /* clear ESOF flag in ISTR */
+            _SetISTR((uint16_t)CLR_ESOF);
 
-        /*poll for RESET flag in ISTR*/
-        while((_GetISTR()&ISTR_RESET) == 0);
-        /* clear RESET flag in ISTR */
-        _SetISTR((uint16_t)CLR_RESET);
+            if ((_GetFNR() & FNR_RXDP) != 0) {
+                /* increment ESOF counter */
+                esof_counter += 1;
 
-       /*restore Enpoints*/
-        for (i=0;i<8;i++)
-        _SetENDPOINT(i, EP[i]);
+                /* test if we enter in ESOF more than 3 times with
+                 * FSUSP =0 and RXDP =1=>> possible missing SUSP flag
+                 */
+                if ((esof_counter > 3) && ((_GetCNTR() & CNTR_FSUSP) == 0)) {
+                    /* this a sequence to apply a force RESET */
 
-        esof_counter = 0;
-      }
-    }
-    else
-    {
-        esof_counter = 0;
-    }
+                    /* Store CNTR value */
+                    wCNTR = _GetCNTR();
 
-    /* resume handling timing is made with ESOFs */
-    Resume(RESUME_ESOF); /* request without change of the machine state */
+                    /* Store endpoints registers status */
+                    for (i=0; i < 8; i++) {
+                        EP[i] = _GetENDPOINT(i);
+                    }
 
-#ifdef ESOF_CALLBACK
-    ESOF_Callback();
-#endif
-  }
-#endif
+                    /* apply FRES */
+                    wCNTR |= CNTR_FRES;
+                    _SetCNTR(wCNTR);
+
+                    /* clear FRES */
+                    wCNTR &= ~CNTR_FRES;
+                    _SetCNTR(wCNTR);
+
+                    /* poll for RESET flag in ISTR */
+                    while((_GetISTR() & ISTR_RESET) == 0);
+                    /* clear RESET flag in ISTR */
+                    _SetISTR((uint16_t)CLR_RESET);
+
+                    /* restore Enpoints */
+                    for (i=0; i < 8; i++) {
+                        _SetENDPOINT(i, EP[i]);
+                    }
+                    esof_counter = 0;
+                }
+            } else {
+                esof_counter = 0;
+            }
+
+            /* resume handling timing is made with ESOFs */
+            Resume(RESUME_ESOF); /* request without change of the machine state */
+
+            #ifdef ESOF_CALLBACK
+                ESOF_Callback();
+            #endif
+        }
+    #endif
 } /* USB_Istr */
 
 
