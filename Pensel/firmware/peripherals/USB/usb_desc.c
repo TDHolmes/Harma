14db027e1f9d9cef15d7cd2a30289e87ec76311f
Tyler Holmes <tylerdanielholmes@gmail.com>
2018-05-27 18:14:28 -0500

    Reformatting descriptor comment structure. No real code change

--- Pensel/firmware/peripherals/USB/usb_desc.c
+++ Pensel/firmware/peripherals/USB/usb_desc.c
@@ -42,116 +42,113 @@
 
 
 /* USB Standard Device Descriptor */
-const uint8_t Virtual_Com_Port_DeviceDescriptor[] =
-  {
-    0x12,   /* bLength */
-    USB_DEVICE_DESCRIPTOR_TYPE,     /* bDescriptorType */
-    0x00,
-    0x02,         /* bcdUSB = 2.00 */
-    0x02,         /* bDeviceClass: CDC */
-    0x02,         /* bDeviceSubClass */
-    0x02,         /* bDeviceProtocol */
-    0x40,         /* bMaxPacketSize0 */
-    0x83, 0x04,   /* idVendor = 0x0483 */
-    0x40, 0x57,   /* idProduct = 0x7540 */
-    0x00,
-    0x02,         /* bcdDevice = 2.00 */
-    1,              /* Index of string descriptor describing manufacturer */
-    2,              /* Index of string descriptor describing product */
-    3,              /* Index of string descriptor describing the device's serial number */
-    0x01          /* bNumConfigurations */
+const uint8_t Virtual_Com_Port_DeviceDescriptor[] = {
+
+    0x12,                       // bLength              Descriptor size (18 bytes)
+    USB_DEVICE_DESCRIPTOR_TYPE, // bDescriptorType      Descriptor type (Device)
+    0x00, 0x02,                 // bcdUSB               USB spec 2.0
+    0x02,                       // bDeviceClass         CDC device class
+    0x02,                       // bDeviceSubClass      Serial coms device
+    0x02,                       // bDeviceProtocol      ?
+    0x40,                       // bMaxPacketSize0      Max packet size on iface 0
+    0x83, 0x04,                 // idVendor             Vendor ID (0x0483)
+    0x40, 0x57,                 // idProduct            Product ID (0x7540)
+    0x00, 0x02,                 // bcdDevice            Device release number (2.00)
+    0x01,                       // iManufacturer        Index of string manufacturer descriptor
+    0x02,                       // iProduct             Index of string describing product
+    0x03,                       // iSerialNumber        Index of string for device's serial number
+    0x01                        // bNumConfigurations
   };
 
 const uint8_t Virtual_Com_Port_ConfigDescriptor[] =
   {
-    /*Configuration Descriptor*/
-    0x09,   /* bLength: Configuration Descriptor size */
-    USB_CONFIGURATION_DESCRIPTOR_TYPE,      /* bDescriptorType: Configuration */
-    VIRTUAL_COM_PORT_SIZ_CONFIG_DESC,       /* wTotalLength:no of returned bytes */
-    0x00,
-    0x02,   /* bNumInterfaces: 2 interface */
-    0x01,   /* bConfigurationValue: Configuration value */
-    0x00,   /* iConfiguration: Index of string descriptor describing the configuration */
-    0xC0,   /* bmAttributes: self powered */
-    0x32,   /* MaxPower 0 mA */
-    /*Interface Descriptor*/
-    0x09,   /* bLength: Interface Descriptor size */
-    USB_INTERFACE_DESCRIPTOR_TYPE,  /* bDescriptorType: Interface */
 
-    /* Interface descriptor type */
-    0x00,   /* bInterfaceNumber: Number of Interface */
-    0x00,   /* bAlternateSetting: Alternate setting */
-    0x01,   /* bNumEndpoints: One endpoints used */
-    0x02,   /* bInterfaceClass: Communication Interface Class */
-    0x02,   /* bInterfaceSubClass: Abstract Control Model */
-    0x01,   /* bInterfaceProtocol: Common AT commands */
-    0x00,   /* iInterface: */
+    // --- Configuration Descriptor
+    0x09,                               // bLength              Configuration Descriptor size
+    USB_CONFIGURATION_DESCRIPTOR_TYPE,  // bDescriptorType      Configuration
+    VIRTUAL_COM_PORT_SIZ_CONFIG_DESC,   // wTotalLength         no of returned bytes in descriptor
+    0x00,                               //                      and subordinate descriptors
+    0x02,                               // bNumInterfaces       2 interfaces
+    0x01,                               // bConfigurationValue  Configuration value
+    0x00,                               // iConfiguration       Index of string describing configuration
+    0xC0,                               // bmAttributes         self powered  //TODO: fix
+    0x32,                               // MaxPower             0 mA
 
-    /* Header Functional Descriptor */
-    0x05,   /* bLength: Endpoint Descriptor size */
-    0x24,   /* bDescriptorType: CS_INTERFACE */
-    0x00,   /* bDescriptorSubtype: Header Func Desc */
-    0x10,   /* bcdCDC: spec release number */
-    0x01,
+    // --- Interface Descriptor
+    0x09,                               // bLength              Interface Descriptor size
+    USB_INTERFACE_DESCRIPTOR_TYPE,      // bDescriptorType      Interface
 
-    /* Call Management Functional Descriptor */
-    0x05,   /* bFunctionLength */
-    0x24,   /* bDescriptorType: CS_INTERFACE */
-    0x01,   /* bDescriptorSubtype: Call Management Func Desc */
-    0x00,   /* bmCapabilities: D0+D1 */
-    0x01,   /* bDataInterface: 1 */
+    //  Interface descriptor type
+    0x00,                               // bInterfaceNumber     Number of Interface
+    0x00,                               // bAlternateSetting    Alternate setting
+    0x01,                               // bNumEndpoints        One endpoints used
+    0x02,                               // bInterfaceClass      Communication Interface Class
+    0x02,                               // bInterfaceSubClass   Abstract Control Model
+    0x01,                               // bInterfaceProtocol   Common AT commands
+    0x00,                               // iInterface           Index of interface? TODO confirm
 
-    /* ACM Functional Descriptor */
-    0x04,   /* bFunctionLength */
-    0x24,   /* bDescriptorType: CS_INTERFACE */
-    0x02,   /* bDescriptorSubtype: Abstract Control Management desc */
-    0x02,   /* bmCapabilities */
+    // Header Functional Descriptor
+    0x05,                               // bLength              Endpoint Descriptor size
+    0x24,                               // bDescriptorType      CS_INTERFACE
+    0x00,                               // bDescriptorSubtype   Header Func Desc
+    0x10, 0x01,                         // bcdCDC               spec release number (1.10)
 
-    /* Union Functional Descriptor */
-    0x05,   /* bFunctionLength */
-    0x24,   /* bDescriptorType: CS_INTERFACE */
-    0x06,   /* bDescriptorSubtype: Union func desc */
-    0x00,   /* bMasterInterface: Communication class interface */
-    0x01,   /* bSlaveInterface0: Data Class Interface */
+    // Call Management Functional Descriptor
+    0x05,                               // bFunctionLength
+    0x24,                               // bDescriptorType      CS_INTERFACE
+    0x01,                               // bDescriptorSubtype   Call Management Func Desc
+    0x00,                               // bmCapabilities       D0+D1
+    0x01,                               // bDataInterface       1
 
-    /* Endpoint 2 Descriptor */
-    0x07,   /* bLength: Endpoint Descriptor size */
-    USB_ENDPOINT_DESCRIPTOR_TYPE,   /* bDescriptorType: Endpoint */
-    0x82,   /* bEndpointAddress: (IN2) */
-    0x03,   /* bmAttributes: Interrupt */
-    VIRTUAL_COM_PORT_INT_SIZE,      /* wMaxPacketSize: */
-    0x00,
-    0xFF,   /* bInterval: */
+    // ACM Functional Descriptor
+    0x04,                               // bFunctionLength
+    0x24,                               // bDescriptorType      CS_INTERFACE
+    0x02,                               // bDescriptorSubtype   Abstract Control Management desc
+    0x02,                               // bmCapabilities       ? TODO figure out
 
-    /* Data class interface descriptor */
-    0x09,   /* bLength: Endpoint Descriptor size */
-    USB_INTERFACE_DESCRIPTOR_TYPE,  /* bDescriptorType: */
-    0x01,   /* bInterfaceNumber: Number of Interface */
-    0x00,   /* bAlternateSetting: Alternate setting */
-    0x02,   /* bNumEndpoints: Two endpoints used */
-    0x0A,   /* bInterfaceClass: CDC */
-    0x00,   /* bInterfaceSubClass: */
-    0x00,   /* bInterfaceProtocol: */
-    0x00,   /* iInterface: */
+    // Union Functional Descriptor
+    0x05,                               // bFunctionLength
+    0x24,                               // bDescriptorType      CS_INTERFACE
+    0x06,                               // bDescriptorSubtype   Union func desc
+    0x00,                               // bMasterInterface     Communication class interface
+    0x01,                               // bSlaveInterface0     Data Class Interface
 
-    /* Endpoint 3 Descriptor */
-    0x07,   /* bLength: Endpoint Descriptor size */
-    USB_ENDPOINT_DESCRIPTOR_TYPE,   /* bDescriptorType: Endpoint */
-    0x03,   /* bEndpointAddress: (OUT3) */
-    0x02,   /* bmAttributes: Bulk */
-    VIRTUAL_COM_PORT_DATA_SIZE,             /* wMaxPacketSize: */
-    0x00,
-    0x00,   /* bInterval: ignore for Bulk transfer */
+    // --- Endpoint 2 Descriptor
+    0x07,                               // bLength              Endpoint Descriptor size
+    USB_ENDPOINT_DESCRIPTOR_TYPE,       // bDescriptorType      Endpoint
+    0x82,                               // bEndpointAddress     (IN2)
+    0x03,                               // bmAttributes         Interrupt
+    VIRTUAL_COM_PORT_INT_SIZE, 0x00,    // wMaxPacketSize
+    0xFF,                               // bInterval
 
-    /* Endpoint 1 Descriptor */
-    0x07,   /* bLength: Endpoint Descriptor size */
-    USB_ENDPOINT_DESCRIPTOR_TYPE,   /* bDescriptorType: Endpoint */
-    0x81,   /* bEndpointAddress: (IN1) */
-    0x02,   /* bmAttributes: Bulk */
-    VIRTUAL_COM_PORT_DATA_SIZE,             /* wMaxPacketSize: */
-    0x00,
-    0x00    /* bInterval */
-  };
+    // --- Data class interface descriptor
+    0x09,                               // bLength              Endpoint Descriptor size
+    USB_INTERFACE_DESCRIPTOR_TYPE,      // bDescriptorType:
+    0x01,                               // bInterfaceNumber     Number of Interface
+    0x00,                               // bAlternateSetting    Alternate setting
+    0x02,                               // bNumEndpoints        Two endpoints used
+    0x0A,                               // bInterfaceClass      CDC
+    0x00,                               // bInterfaceSubClass
+    0x00,                               // bInterfaceProtocol
+    0x00,                               // iInterface
+
+    // --- Endpoint 3 Descriptor
+    0x07,                               // bLength              Endpoint Descriptor size
+    USB_ENDPOINT_DESCRIPTOR_TYPE,       // bDescriptorType      Endpoint
+    0x03,                               // bEndpointAddress     (OUT3)
+    0x02,                               // bmAttributes         Bulk
+    VIRTUAL_COM_PORT_DATA_SIZE, 0x00,   // wMaxPacketSize
+    0x00,                               // bInterval            ignore for Bulk transfer
+
+    // --- Endpoint 1 Descriptor
+    0x07,                               // bLength              Endpoint Descriptor size
+    USB_ENDPOINT_DESCRIPTOR_TYPE,       // bDescriptorType      Endpoint
+    0x81,                               // bEndpointAddress     (IN1)
+    0x02,                               // bmAttributes         Bulk
+    VIRTUAL_COM_PORT_DATA_SIZE, 0x00,   // wMaxPacketSize
+    0x00                                // bInterval
+};
+
 
 /* USB String Descriptors */
 const uint8_t Virtual_Com_Port_StringLangID[VIRTUAL_COM_PORT_SIZ_STRING_LANGID] =
