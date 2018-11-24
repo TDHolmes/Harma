af3f724ed169d641332b58a90cfbf37b32d75674
Tyler Holmes <tylerdanielholmes@gmail.com>
2018-11-24 15:16:49 +0000

    minor logging changes

--- Pensel/firmware/modules/utilities/logging.c
+++ Pensel/firmware/modules/utilities/logging.c
@@ -6,8 +6,6 @@
 #include <stdio.h>
 #include <string.h>
 
-#define MAX_LOG_LEN (512)
-
 typedef struct log_admin {
     log_level_t log_level;
     ret_t (*write_func)(char *);
@@ -15,7 +13,11 @@ typedef struct log_admin {
 } log_admin_t;
 
 static log_admin_t gLogAdmin;
-static char gMessageBuffer[MAX_LOG_LEN];
+static char gMessageBuffer[LOGGING_MAX_MSG_LEN];
+
+
+int populateLogLevel(log_level_t level, char buffer_ptr[]);
+
 
 /*! Initializes the logging module with some infoooo
  */
@@ -28,6 +30,40 @@ ret_t log_init(log_level_t level, ret_t (*write_func_ptr)(char *),
 
     return RET_OK;
 }
+
+
+/*! Log a log message at the given level and location in the code.
+ *
+ */
+ret_t log_logMessage(log_level_t level, const char filename[], const char funcname[],
+                     uint32_t linenum, const char msg_ptr[])
+{
+    int logLevelBytes;
+    int finalLogBytes;
+    if (level > gLogAdmin.log_level) {
+        // Message more verbose than our set level. Don't do anything
+        return RET_OK;
+    }
+
+    logLevelBytes = populateLogLevel(level, gMessageBuffer);
+    if (logLevelBytes <= 0) {
+        return RET_NOMEM_ERR;
+    }
+    finalLogBytes = snprintf(gMessageBuffer + logLevelBytes, sizeof(gMessageBuffer) - logLevelBytes,
+                             "%s:%u (in %s): %s\n", filename, linenum, funcname, msg_ptr);
+
+    if (finalLogBytes <= 0 || finalLogBytes + logLevelBytes >= (int)sizeof(gMessageBuffer)) {
+        return RET_NOMEM_ERR;
+    }
+
+    return gLogAdmin.write_func(gMessageBuffer);
+}
+
+void log_changeLevel(log_level_t newLevel) {
+    gLogAdmin.log_level = newLevel;
+}
+
+/* --- Private Helper Functions --- */
 
 /*! Given a log level and string buffer to write into, write in the log level
  *    and current time. Return number of bytes this takes up.
@@ -56,31 +92,4 @@ int populateLogLevel(log_level_t level, char buffer_ptr[])
     }
 
     return sprintf(buffer_ptr, "%08ums - %s ", curTime_ms, logLevelMsg);
-}
-
-/*! Log a log message at the given level and location in the code.
- *
- */
-ret_t log_logMessage(log_level_t level, const char filename[], const char funcname[],
-                     uint32_t linenum, const char msg_ptr[])
-{
-    int logLevelBytes;
-    int finalLogBytes;
-    if (level > gLogAdmin.log_level) {
-        // Message more verbose than our set level. Don't do anything
-        return RET_OK;
-    }
-
-    logLevelBytes = populateLogLevel(level, gMessageBuffer);
-    if (logLevelBytes <= 0) {
-        return RET_NOMEM_ERR;
-    }
-    finalLogBytes = snprintf(gMessageBuffer + logLevelBytes, sizeof(gMessageBuffer) - logLevelBytes,
-                             "%s:%u (in %s): %s\n", filename, linenum, funcname, msg_ptr);
-
-    if (finalLogBytes <= 0 || finalLogBytes + logLevelBytes >= (int)sizeof(gMessageBuffer)) {
-        return RET_NOMEM_ERR;
-    }
-
-    return gLogAdmin.write_func(gMessageBuffer);
 }
--- Pensel/firmware/modules/utilities/logging.h
+++ Pensel/firmware/modules/utilities/logging.h
@@ -2,8 +2,10 @@
  *
  */
 #include <stdint.h>
-
+#include <stdio.h>
 #include "common.h"
+
+#define LOGGING_MAX_MSG_LEN (512)
 
 typedef enum {
     kLogLevelError,   //!< Error level (catastrophic failure)
@@ -12,15 +14,27 @@ typedef enum {
     kLogLevelDebug    //!< Debug level (verbose debugging information)
 } log_level_t;
 
-#define LOG_MSG(__LVL__, __MSG__) log_logMessage(__LVL__, __FILE__, __func__, __LINE__, __MSG__)
+#define LOG_MSG(__LVL__, __MSG__) log_logMessage(__LVL__, __BASE_FILE__, __func__, __LINE__, __MSG__)
 
-#define LOG_MSG_FMT(__LVL__, __MSG__, ...)   \
-    char msg_buff[64];                       \
-    sprintf(msg_buff, __MSG__, __VA_ARGS__); \
-    LOG_MSG(__LVL__, msg_buff)
+#define LOG_MSG_FMT(__LVL__, __MSG__, ...)                                \
+    char __log_msg_fmt_buff[LOGGING_MAX_MSG_LEN];                         \
+    snprintf(__log_msg_fmt_buff, sizeof(__log_msg_fmt_buff), __MSG__, __VA_ARGS__); \
+    LOG_MSG(__LVL__, __log_msg_fmt_buff)
 
+// TODO: document!
+/*!
+ *
+ */
 ret_t log_init(log_level_t level, ret_t (*write_func_ptr)(char *),
                uint32_t (*get_cur_time_ptr)(void));
 
+/*!
+ *
+ */
+void log_changeLevel(log_level_t newLevel);
+
+/*!
+ *
+ */
 ret_t log_logMessage(log_level_t level, const char filename[], const char funcname[],
                      uint32_t linenum, const char msg_ptr[]);
